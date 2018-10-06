ad420ce423e8dd0c6f5ff0ccc534b32a5626a014
cerberus-ab <cerberus.ab@mail.ru>
2018-10-06 23:26:07 +0300

    Initial implementation

--- spec/jsoa.spec.js
+++ spec/jsoa.spec.js
@@ -3,14 +3,106 @@
 const assert = require('assert');
 const JSOA = require('../src/jsoa');
 
+// TODO: much better do not rely on order
+const assertObjectEqual = (o1, o2) => 
+    assert.strictEqual(JSON.stringify(o1), JSON.stringify(o2));
+
 describe('JavaScript Object Analyzer specification', () => {
    
     describe('the module', () => {
+        it('should contain number of current version', () => {
+            assert.ok(typeof JSOA.VERSION == 'string'); 
+        });
+        it('should provide a function for getting tag: getTag', () => {
+            assert.ok(typeof JSOA.inspFlat == 'function');
+        });
         it('should provide a function for flat inspection: inspFlat', () => {
             assert.ok(typeof JSOA.inspFlat == 'function');
         });
         it('should provide a function for deep inspection: inspDeep', () => {
             assert.ok(typeof JSOA.inspDeep == 'function');
+        });
+    });
+    
+    describe('getTag: Get the tag of a variable', () => {
+        it('should pay attention on Symbol.toStringTag firstly', () => {
+            let kitten = {
+                [Symbol.toStringTag]: 'Kitten'
+            };
+            class Cat {
+                get [Symbol.toStringTag]() {
+                    return 'Kitten';
+                }
+            };
+            assert.strictEqual(JSOA.getTag(kitten), 'Kitten');
+            assert.strictEqual(JSOA.getTag(new Cat), 'Kitten');
+        });
+        it('should pay attention on constructor.name for objects secondly', () => {
+            function Dog() {};
+            class Cat {};
+            assert.strictEqual(JSOA.getTag(new Dog), 'Dog');
+            assert.strictEqual(JSOA.getTag(new Cat), 'Cat');
+        }); 
+        it('should consider separately: null, undefined, NaN', () => {
+            assert.strictEqual(JSOA.getTag(null), 'Null');
+            assert.strictEqual(JSOA.getTag(undefined), 'Undefined');
+            assert.strictEqual(JSOA.getTag(NaN), 'Number');
+        });
+    });
+    
+    describe('inspFlat: Flat inspection cases', () => {
+        it('should require a plain or iterable object', () => {
+            assert.throws(() => JSOA.inspFlat('seed'), TypeError);
+            assert.throws(() => JSOA.inspFlat(null), TypeError);
+        });
+        it('should collect stats over a plain object properties', () => {
+            assertObjectEqual(JSOA.inspFlat({ x: 10, y: 20, exists: true, name: 'p1' }), { 
+                Number: 2, Boolean: 1, String: 1 
+            });
+        });
+        it('should collect stats over Array', () => {
+            assertObjectEqual(JSOA.inspFlat([ Infinity, x => 0, new Date ]), {
+                Number: 1, Function: 1, Date: 1 
+            });
+        });
+        it('should collect stats over Set/Map', () => {
+            assertObjectEqual(JSOA.inspFlat(new Map([['a', 1], ['b', 2], ['c', 3]])), {
+                Array: 3 
+            });
+            assertObjectEqual(JSOA.inspFlat(new Set([1, 1, 2, 2, 3])), {
+                Number: 3 
+            });
+        });
+        it('should collect stats over any iterable objects', () => {
+            let iterable = {
+                [Symbol.iterator]() {
+                    return {
+                        i: 0,
+                        next() {
+                            if (this.i < 10) {
+                                return { value: this.i++, done: false };
+                            }
+                            return { value: undefined, done: true };
+                        }
+                    };
+                }
+            };
+            assertObjectEqual(JSOA.inspFlat(iterable), {
+                Number: 10
+            });
+        });
+        it('should consider nested plain and iterable objects flatly', () => {
+            assertObjectEqual(JSOA.inspFlat({ obj: { x: 10 }, arr: [1, 2, 3] }), {
+                Object: 1, Array: 1
+            });
+        });
+    });
+    
+    describe('inspDeep: Deep inspection cases', () => {
+        it('should collect stats recursively over plain and iterable objects', () => {
+            assertObjectEqual(JSOA.inspDeep({ obj: { x: 10 }, arr: [1, 2, [3, 4]] }), {
+                Number: 5
+            });
         });
     });
     
--- src/jsoa.js
+++ src/jsoa.js
@@ -1,17 +1,142 @@
 ;(function() {
     
-    function inspFlat(obj) {
-        // TODO: to implement
+    var VERSION = '0.1.1';
+    
+    /**
+     * Get Tag via Symbol.toStringTag, Function.name 
+     * or default toString method of Object.
+     * 
+     * @param {Mixed} any
+     * @returns {string}
+     */
+    function getTag(any) {
+        if (isObject(any)) {
+            if (typeof any[Symbol.toStringTag] === 'string') {
+                return any[Symbol.toStringTag];
+            }
+            if (typeof any.constructor === 'function' && typeof any.constructor.name === 'string') {
+                return any.constructor.name;
+            }
+        }
+        return Object.prototype.toString.call(any).match(/\[object\s(\w+)]/)[1];
     }
     
+    /**
+     * Predicates for checking a variable for:
+     * object, plain object and iterable object.
+     * 
+     */
+    function isObject(any) {
+        return typeof any === 'object' && any !== null;
+    }
+    function isPlainObject(any) {
+        return isObject(any) && any.constructor === Object;
+    }
+    function isIterable(any) {
+        return isObject(any) && typeof any[Symbol.iterator] === 'function';
+    }
+    
+    /**
+     * Returns iterable seed of a variable if it`s possible.
+     * 
+     * @param {Mixed} any
+     * @returns {object|undefined}
+     */
+    function toIterable(any) {
+        if (isIterable(any)) {
+            return any;
+        }
+        if (isPlainObject(any)) {
+            return Object.keys(any).map(function(k) { 
+                return any[k];
+            });
+        }
+    }
+    
+    /**
+     * Group and count elements in array.
+     * 
+     * @param {Array<string>} array
+     * @returns {object}
+     */
+    function groupCount(array) {
+        return array.reduce(function(res, el) {
+            res[el] = (res[el] || 0) + 1;
+            return res;
+        }, {});
+    }
+    
+    /**
+     * Creates reducer for collecting and resulting tags collection;
+     * provides add and combine methods respectively.
+     *
+     * @param {function} combiner
+     * @returns {object}
+     */
+    function initTagsReducer(combiner) {
+        var tags = [];
+        return {
+            add: function(tag) {
+                tags.push(tag);
+            },
+            combine: function() {
+                return combiner(tags);
+            }
+        };
+    }
+    
+    /**
+     * Recursively iterates over an iterable object 
+     * using reducer for accumulating the data.
+     *
+     * @param {object} iterable
+     * @param {object} reducer, Should be created by initTagsReducer
+     * @param {boolean} deep, Set true for deep iterating
+     */
+    function iterOver(iterable, reducer, deep) {
+        // TODO: check for compatibility
+        for (var it of iterable) {
+            var nextIterable = toIterable(it);
+            if (!(deep && nextIterable)) {
+                reducer.add(getTag(it));
+            } else {
+                iterOver(nextIterable, reducer, deep);
+            }
+        }
+    }
+    
+    /**
+     * Collects statistics over a plain or iterable object.
+     *
+     * @param {object} obj
+     * @param {boolean} deep, Set true for collecting data in nested objects
+     */
+    function inspObject(obj, deep) {
+        var iterable = toIterable(obj);
+        if (!iterable) {
+            throw new TypeError(obj + ' isn`t and can`t be iterable');
+        }
+        var reducer = initTagsReducer(groupCount);
+        iterOver(iterable, reducer, Boolean(deep));
+        return reducer.combine();
+    }
+    
+    // Flat inspection
+    function inspFlat(obj) {
+        return inspObject(obj);
+    }
+    
+    // Deep inspection
     function inspDeep(obj) {
-        // TODO: to implement
+        return inspObject(obj, true);
     }
     
     // the module exports
     var JSOA = {
+        VERSION: VERSION,
+        getTag: getTag,
         inspFlat: inspFlat,
-        inspDeep: inspDeep
+        inspDeep: inspDeep    
     };
 
     // define the module as AMD, commonJS or global
